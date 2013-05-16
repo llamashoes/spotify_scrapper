6ace65c32e352c33056157234f32e78137f8407a
Ryan Gifford <rgifford@gmail.com>
2013-05-16 11:51:36 -0600

    Added code to loop through a file containing spotify urls.

--- test.rb
+++ test.rb
@@ -1,14 +1,27 @@
+# Requires fvxb to installed. Also the headless gem
+# Pass in the file with the spotify urls. Each URL on a separate link
+
 require 'rubygems'
 require 'watir'
 require 'watir-webdriver'
+require 'headless'
 
+headless = Headless.new
+headless.start
 browser = Watir::Browser.new
-browser.goto 'http://open.spotify.com/track/6IK8OzOAHZrTGa9LkVcgx4'
-sleep(2)
 
-song_title = browser.h1(:xpath, "//*[@id=\"main\"]/div/div/div[2]/div[1]/h1").text
-artist = browser.a(:xpath, "//*[@id=\"main\"]/div/div/div[2]/div[1]/h2/a").text
-album = browser.a(:xpath, "//*[@id=\"main\"]/div/div/div[2]/h3/a").text
-puts song_title + " : " + artist + " : " + album
-browser.close()
+file = File.new(ARGV[0], "r")
 
+file.each do |l|
+
+	browser.goto l
+	#sleep(2)
+
+	song_title = browser.h1(:xpath, "//*[@id=\"main\"]/div/div/div[2]/div[1]/h1").text
+	artist = browser.a(:xpath, "//*[@id=\"main\"]/div/div/div[2]/div[1]/h2/a").text
+	album = browser.a(:xpath, "//*[@id=\"main\"]/div/div/div[2]/h3/a").text
+	puts song_title + " : " + artist + " : " + album
+	
+end
+
+headless.destroy
