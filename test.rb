4947c1fe4b3cef30699f6660c3ef632a398e71ad
Ryan Gifford <rgifford@gmail.com>
2013-05-16 11:22:25 -0600

    Updating

--- test.rb
+++ test.rb
@@ -7,6 +7,8 @@ browser.goto 'http://open.spotify.com/track/6IK8OzOAHZrTGa9LkVcgx4'
 sleep(2)
 
 song_title = browser.h1(:xpath, "//*[@id=\"main\"]/div/div/div[2]/div[1]/h1").text
-puts song_title
+artist = browser.a(:xpath, "//*[@id=\"main\"]/div/div/div[2]/div[1]/h2/a").text
+album = browser.a(:xpath, "//*[@id=\"main\"]/div/div/div[2]/h3/a").text
+puts song_title + " : " + artist + " : " + album
 browser.close()
 
